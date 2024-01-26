d5c3a7dd6943db23a2b94fc919dbe92e54f0f5b8
Andrew Zolensky <andrewlzolensky@gmail.com>
2024-01-26 03:00:07 -0500

    set up basic routing

--- client/src/CreatePost.js
+++ client/src/CreatePost.js
@@ -1,7 +1,14 @@
+import React from 'react';
+import Post from './CreatePost.module.css';
+import {Button} from "react-bootstrap";
+import {useNavigate} from "react-router-dom";
+
 function CreatePost() {
+    const navigate = useNavigate();
     return(
-        <div>
+        <div className={Post.post}>
             <h1>Create a Post</h1>
+            <Button onClick={() => navigate(-1)}>BACK</Button>
         </div>
     )
 }
